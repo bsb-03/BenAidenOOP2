97f3b3a3f60a7a4584a677b2de3552abd3899e0e
asharrison <asharrison97@gmail.com>
2025-03-05 16:44:24 -0600

    Added javadoc code within MiddleEarthCouncil.java file.

--- eclipse-workspace/OOPAssignment2/src/a2/middleearthcouncil/MiddleEarthCouncil.java
+++ eclipse-workspace/OOPAssignment2/src/a2/middleearthcouncil/MiddleEarthCouncil.java
@@ -1,10 +1,21 @@
 package a2.middleearthcouncil;
 import a2.charactermanager.*;
 
+/**
+ * MiddleEarthCouncil class - Singleton class designed to provide access to the CharacterManager class. Singleton design pattern
+ * ensures that only one instance of MiddleEarthCouncil will exist at any given time. Has two fields, one for the MiddleEarthCouncil
+ * instance itself, and one to provide access to the CharacterManager class.
+ */
 public class MiddleEarthCouncil {
 	private static MiddleEarthCouncil instance;
 	private static CharacterManager manager;
 	
+	/**
+	 * getInstance method - Checks whether or not an instance of MiddleEarthCouncil exists. 
+	 * If an instance is found, method returns the MiddleEarthCouncil instance. If it does not exist, 
+	 * a new instance is created and returned.
+	 * @return instance
+	 */
 	public static MiddleEarthCouncil getInstance() {
 		if (instance == null)
 			instance = new MiddleEarthCouncil();
