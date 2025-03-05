426a24364495a2f4a327107acfa0e36d078a357b
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-03-05 13:23:28 -0600

    started on main menu

--- eclipse-workspace/OOPAssignment2/src/a2/menu/Menu.java
+++ eclipse-workspace/OOPAssignment2/src/a2/menu/Menu.java
@@ -2,29 +2,35 @@ package a2.menu;
 import a2.charactermanager.*;
 import a2.middleearthcharacter.*;
 import a2.middleearthcouncil.*;
+import java.util.Scanner;
 
 public class Menu {
+	
+	public static void printMenu() {
+		System.out.println("---MENU---\n");
+		
+		System.out.println(	"1. Add new character\n"
+							+ "2. View all characters\n"
+							+ "3. Update a character\n"
+							+ "4. Delete a character\n"
+							+ "5. Execute all attack actions\n"
+							+ "6. Exit");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("---TESTING---");
+		boolean exitFlag = false;
+		int userChoice = 0;
+		Scanner scanner = new Scanner(System.in);
 		
-		CharacterManager manager = new CharacterManager();
-		
-		Elf myElf = new Elf();
-		myElf.setHealth(1000);
-		myElf.setPower(20);
-		myElf.setName("Elfo");
-		
-		Dwarf myDwarf = new Dwarf();
-		myDwarf.setHealth(1500);
-		myDwarf.setPower(15);
-		myDwarf.setName("Dwarfo");
-		
-		//myElf.displayInfo();
-		
-		manager.addCharacter(myElf);
-		manager.addCharacter(myDwarf);
-		
-		manager.displayAllCharacters();
+		while(!exitFlag) {
+			printMenu();
+			
+			System.out.println(">>> ");
+			userChoice = Integer.parseInt(scanner.nextLine());
+			
+			if(userChoice == 6) {
+				exitFlag = true;
+			}
+		}
 	}
 }
